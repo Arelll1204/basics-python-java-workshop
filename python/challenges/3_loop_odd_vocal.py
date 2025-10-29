760ba5f44aea11f99e7f2c239012146fd19e2f3a
Mac <mac@MacBook-Air-arell.local>
2025-10-29 10:18:01 +0700

    test push

--- python/challenges/3_loop_odd_vocal.py
+++ python/challenges/3_loop_odd_vocal.py
@@ -1,11 +1,18 @@
 # TODO: Program untuk mencetak bilangan ganjil dari 1 hingga 15
 odd_numbers = []
-print(odd_numbers)
+for i in range(1, 16, 2):  # Mulai dari 1, sampai 15, step 2
+    odd_numbers.append(i)
+
+print("Bilangan ganjil dari 1 hingga 15:", odd_numbers)
 
 # TODO: Program menghitung jumlah huruf vokal
 word = input("Masukkan kata: ").lower()
 
-vowels = "aiueo"
+vowels = "aiueo"  # Diperbaiki dari "aimeo" menjadi "aiueo"
 count = 0
+
+for char in word:
+    if char in vowels:
+        count += 1
 
 print("Jumlah huruf vokal:", count)
