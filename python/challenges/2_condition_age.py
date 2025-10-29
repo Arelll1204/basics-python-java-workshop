3e87e36970d11ea5356c2edb82766e5a31fd57c2
Mac <mac@MacBook-Air-arell.local>
2025-10-29 10:56:53 +0700

    testing

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,17 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
-
+age = int(input("Masukkan umur anda: "))
 # 2. TODO Tentukan kategori berdasarkan usia
+if age < 0:
+    kategori = "Usia tidak valid"
+elif age < 13:
+    kategori = "Anak-anak"
+elif age < 18:
+    kategori = "Remaja"
+elif age < 60:
+    kategori = "Dewasa" 
+else:
+    kategori = "Lansia"
+
+print("Usia anda adalah", kategori)
